4b90dbddc6c71d7ea15553a13eb055edce7965fc
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-12-17 16:44:35 -0600

    Create Reference2IP.go

---
+++ Reference2IP.go
@@ -0,0 +1,30 @@
+//this is a refrence
+
+package main 
+import("fmt"
+       "net"
+       "os")
+
+func main(){
+
+    if len(os.Args) !=2{
+
+       fmt.Fprintf(os.Stderr,"this supposed to make a homemade IP address")
+       os.Exit(1)
+
+    }
+
+    name  := os.Args[1]
+
+    addr := net.ParseIP(name)
+    if addr == nil{
+
+         fmt.Println("NOT VALID")
+
+    }else{
+
+         fmt.Println("VALID ADDY", addr.String())
+
+    }
+    os.Exit(0)
+}
