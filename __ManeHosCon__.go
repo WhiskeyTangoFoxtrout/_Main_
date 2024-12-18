f18570510c10957a0b8a5f34c6170ba02dcbe150
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-12-18 13:44:42 -0600

    Create __ManeHosCon__.go

---
+++ __ManeHosCon__.go
@@ -0,0 +1,15 @@
+//ima try and make this a config file for __HostMane__\
+//Felt like it would be better to import this like a package... Mane starting ta drag it
+
+package main
+
+import("fmt"
+       "net")
+
+type ListenConfig struct{
+
+    control func(IPv4, "49,7,10,15" string, p syscall.RawConn) error
+    KeepAliveConfig KeepAliveConfig //this can be ignored if you setIt.Enable(false)//idk
+    Nosy (Ear *ListenConfig) Listen(ctx context.Context, "IP","49,7,10,15")(Nosy, error)//idfk
+  
+}
