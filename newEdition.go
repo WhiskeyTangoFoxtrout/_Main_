74813be990000673298091ee27bf191c85b5bc8f
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-12-26 10:52:39 -0600

    Create newEdition.go

---
+++ newEdition.go
@@ -0,0 +1,23 @@
+//Hopin this telephone my mane
+
+package main
+
+import("fmt"
+       "net")
+
+func main(){
+
+    goto lips
+  
+}
+
+func lips(){
+
+    conn, err := net.Dial("ipv4","Mainbackend.abc:80")
+    if err != nil {
+
+        os.Exit(1)//this should call 
+      
+    }
+  
+}
