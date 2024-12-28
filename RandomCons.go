d824c9986402cef851670d69b34eae77b2b53978
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2024-12-28 10:23:34 -0600

    Create RandomCons.go

---
+++ RandomCons.go
@@ -0,0 +1,14 @@
+//I just feel like ima need some of these configs. Why is this mostly configs gahlee
+
+type time struct{
+
+    //
+  
+}
+
+type timer struct{
+
+    Sw := time.Timer(64 * time.Second)
+    
+  
+}
