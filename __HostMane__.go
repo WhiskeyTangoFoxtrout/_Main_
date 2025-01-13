f94a4536ce9a12c7dbe6c1070f8b6cd56ae8e733
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-13 12:42:26 -0600

    Update __HostMane__.go

--- __HostMane__.go
+++ __HostMane__.go
@@ -20,10 +20,7 @@ func main(){
     earConfig := Ear
 
     goto ear()
-    http.HandleFunc("/", handler)//http://localhost:52
-    http.HandleFunc("/view/",viewHandler) This should display a website and the title of the website from the URL
-    log.Fatal(http.ListenAndServer(":52",nil))//need to work on ports
-    
+   
 }
 
 type IP []byte
@@ -43,20 +40,6 @@ type Page Struct{
 }
 
 //ima need a save func but idk what ima save just yet. I just want it to echo for rn
-
-func handler( r *http.Request){
-
-    fmt.Fprintf(w "This is the Users BackEnd")//I think this will write out the URL
-    
-}
-
-func ViewHandler(w http.ResponseWriter, t*http.Request){//Pops up on Host server
-
-    title := r.URL.Path[len("/view/"):] //this the title is the URL Path you can see on a website
-    p, _loadPage(title)
-    fmt.Fprintf(w, "<h1>s%</h1><div>%s</div>", p.Title, p.Body)//crude this helps make a website and displays the title
-    
-}
 
 func ear(){//Ok so open port and have port listen for request and respond back with an "WE'RE LIVE FROM DA UNDAGROUND"
      
