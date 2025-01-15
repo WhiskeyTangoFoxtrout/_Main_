f9d35a52567147abe25649cc2e64293ed0fdbbd5
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-15 11:23:02 -0600

    Create port.go

---
+++ port.go
@@ -0,0 +1,27 @@
+//this my port stuff think ima need a couple, one of IP host-host, host-cli, cli-cli
+
+package main
+
+import("fmt"
+       "bytes"
+       "io"
+       "net"
+       "os")
+
+func main(){
+
+    
+    checkError(err)
+    // number 1 is meant for host to host
+    var msg [512]byte
+    msg[0] = //think the protocol number dictate the port 
+    msg[1] =   //code 0
+    msg[2] = 
+    msg[3] = 
+    msg[4] = 
+    msg[5] =  //mite b impotant
+    msg[6] = 
+    msg[7] = //mite be impotant
+
+  
+}
