c254c2bfde05856b43db846fabe97fcbfe731838
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-18 17:05:56 -0600

    Update deWalt.go

--- deWalt.go
+++ deWalt.go
@@ -4,5 +4,16 @@ package main
 import( "fmt"
         "io"
         "os"
-        "github.com/__Main__/hood/")
+        "github.com/__Main__/hood/unda/ports.go"
+        "github.com/__Main__/hood/unda/keyStone.go"
+        "github.com/__Main__/hood/unda/CoCon.go")
+
+func main(){
+
+
+    nil//think this really should start with a byte reader to get a dictioary and store the req info frm mainMachi
+        //need to read(or call the parser ffrom keyStone) and look for the port#mite need an arry 
+    //
+}
+
 
