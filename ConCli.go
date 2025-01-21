877c18a6e121c1030d52c08ade0243fa3e6642da
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-21 11:27:14 -0600

    Update ConCli.go

--- ConCli.go
+++ ConCli.go
@@ -1,11 +1,28 @@
-type JohnLo interface{//****add DNS look up and a way to communicate to Login Page
+type Johnreach interface{//****add DNS look up and a way to communicate to Login Page, this a conn
 
-    Read(IP []byte)(n int, err error)
-    Write(IP []byte)(n int, err error)
-    Close() error
-    Lo() Addr
-    remoteLo() Addr
-    setDeadline(t time.Time) error
-    setWriteDeadline(t time.Time) error
-  
+    Read(IP []byte)(n int, err error)//transfer
+    Write(IP []byte)(n int, err error)//transfer
+    Close() error//ends conn
+    Lo() Addr //mo litteral
+    remoteLo() Addr //this b mo of reference
+    Timeout (c time.Time) error 
+    readTimeout (c time.Time) error
+    
+}
+
+//I need to figure a way to get the IpandSocket sent to Dewalt
+
+//need a config for the addr
+type PnI interface{
+
+    type PoIp struct{
+
+    IP IP
+    IPv4 string
+    IPv6 string
+    port string//trying to put the port and IP together
+    func JoinHostPort([],port string) string//this makes addr that can be sent to other port'd machine
+        
+    }
+    
 }
