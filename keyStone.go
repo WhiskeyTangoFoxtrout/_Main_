69b93f80badb359b582b7a3597e7936251efa72b
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-21 16:33:22 -0600

    Update keyStone.go

--- keyStone.go
+++ keyStone.go
@@ -1,5 +1,6 @@
 package main
 //pipeln really 2 the socket(preSoc) its so its can be more automative and opt filled. still need to set up an GUI
+//this gotta put the ip:port read/btreak it apart/read port/parse ip/ check ip location(if im good enough)/connport to
 import (
         "fmt"
         "sys"
