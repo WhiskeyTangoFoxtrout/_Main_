7fa9174a9684afe89a6e35920e94995531d0f3e6
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-21 11:27:10 -0600

    Update newEdition.go

--- newEdition.go
+++ newEdition.go
@@ -5,39 +5,53 @@ package main
 import("fmt"
        "net"
        "github.com/main/WorkSite/InitViewPage-Cli.css"//think this links it
+       "github.com/Configs"
        "os"
       )
 
+ var Johnreach() := jon
+
  var globalDNSLookup = nil //still trynna figure a method where the user inputs a web address and I can look it up or convert it
 
- var postBox = []
+ var postBox = []//this input from the webpage
 
 func main(){
 
     if logn = 0 {
         
         goto lips    
-    }else{searchBar(*usrNput)}
+    }else{searchBar(*usrNput)}//search bar is to look for other pages on the system...so window and fileDir
     
 }
 
-func lips(){
+//********************************************************************BackNFuncs
 
-    conn, err := net.Dial("ipv6","Mainbackend.abc:80")
+func Lo()
+
+//***************************************************this is mo front-back
+
+func lips(){//talk to em. let em kno who u lookin4
+
+    jon, err := net.Dial(usrNputAddr)// need to make an conn realquick
     if err != nil {
 
         os.Exit(1)//this should call 
       
     }
 
-       conn, err := ln.Accept()
+       jon, err := ln.Accept()
        if err != nil {
               
               os.Exit(1)
               
        }
-       go handleConnection(conn)
+       go handleConnection(jon)
 }
+
+
+
+
+//**************************************************This is more webstuff********************************************
 
 func searchBar(r *Resolver){//**********************************WORK ON DIS*************************************
 
