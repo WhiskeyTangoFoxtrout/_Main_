508834ebf8e6cbff625cf20b7277792f875e9184
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-28 11:57:33 -0600

    Update ports.go

--- ports.go
+++ ports.go
@@ -27,7 +27,7 @@ type sockWrk interface{//dont think this work fast. RIckyBoBBy
     //since this a custom port do I need a number or a name?
         checkError(err)
         // number 1 is meant for host to host
-        var msg [128]byte
+        var msg [128]byte//65
         msg[0] = 8//think the protocol number dictate the port this dictiate the port functionality 8 is host to host pure data transfer
         msg[1] = 0  //code 0 GIVES AN options for ipv6
         msg[2] = 0
@@ -35,32 +35,32 @@ type sockWrk interface{//dont think this work fast. RIckyBoBBy
         msg[4] = 0
         msg[5] = 4 //mite b impotant identifyer
         msg[6] = 0
-        msg[7] = 4//mite be impotant
+        msg[7] = 4//mite be impotant//this is ipv4 if get it right
         len := 8
 
         check16 := check16Sum(msg[0:len])
         msg[2] := byte(check >> 8)//make sure its over the identifier
-        msg[3] := byte(check & 64) //wrkn onnat one
+        msg[3] := byte(check && 64) //wrkn onnat one feel like this a assec check
 
         }
 
        func SwitchBoard( swG int *manifesto []byte){ //this mo host to hose
 
               checkError(err)
-              var keystnPipeln [512]byte
+              var keystnPipeln [256]byte//42
               keystnPipeln[0] = 28//this is made for biggerdata transfer host ot host
               keystnPipeln[1] = 0
               keystnPipeln[2] = 0
               keystnPipeln[3] = 0
               keystnPipeln[4] = 0
-              keystnPipeln[5] = 4
+              keystnPipeln[5] = 4//ip i think
               keystnPipeln[6] = 0
               keystnPipeln[7] = 4
               len := 8
 
               check32 := check32Sum(keystnPipeln[0:8])
               keystnPipeln[2] := byte(check >> 28)
-              keystnPipeln[3] := byte(check & 255)
+              keystnPipeln[3] := byte(check && 168)
               
        }
 
@@ -79,7 +79,7 @@ type sockWrk interface{//dont think this work fast. RIckyBoBBy
 
               check16Sum(KeystnPipeln[0:8])
               KeystnPipeln[2] := byte(check >> 27)
-              KeystnPipeln[3] := byte(check & 62) 
+              KeystnPipeln[3] := byte(check && 69) 
               
        }
            
