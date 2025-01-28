22395dd2d060dc30e8d6ad82efa15540564d32c8
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-28 12:36:54 -0600

    Create offsiteRigg.go

---
+++ offsiteRigg.go
@@ -0,0 +1,14 @@
+//betta to build a server for this. See how well I can pipeln the keyStone
+
+//this gone be a host server
+
+package main
+
+import("fmt"
+       "os"
+       "net"
+       "github.com//host/hood/unda/Configs/ports.go"
+       "github.com/host/hood/unda/Configs/ConCli.go"
+       "github.com/host/hood/unda/Configs/CommonFigs.go"
+    )
+
