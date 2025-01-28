51a6f776f95dd02c5b9e29c1143085bf08068367
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-28 14:34:21 -0600

    Update ConCli.go

--- ConCli.go
+++ ConCli.go
@@ -10,6 +10,17 @@ type Johnreach interface{//****add DNS look up and a way to communicate to Login
     
 }
 
+type earConfig struct{//I forgot wat this for
+    //addr/something else,listen fo da lo to drop
+    KeepAliveConfig KeepAliveConfig //this can be ignored if you setIt.Enable(false)//idk
+    Nosy (Ear *ListenConfig) Listen(ctx context.Context, "IP","49,7,10,15")(Nosy, error)//idfk
+    IP IP //set it to Ip might changeto TCP if I figure packets.
+    Mask IPMask // think this mask it
+    const(IPv4len = 4, IPv6 = 16)//syntax mite whoop me
+       
+}
+
+
 //I need to figure a way to get the IpandSocket sent to Dewalt
 
 //need a config for the addr
