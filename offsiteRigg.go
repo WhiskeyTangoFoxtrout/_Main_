668859b6d30bb72acf654dabb8ec56719d501e2e
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-30 16:01:40 -0600

    Update offsiteRigg.go

--- offsiteRigg.go
+++ offsiteRigg.go
@@ -29,8 +29,24 @@ func main(){
 /*
 engine
 */
-func Pin(){
+func Pin(){//this the ip/dns
 
 
        
+}
+
+func clerk(){//this whats gone receive and write down in an array
+//thinkin this gotta break down bites or files packes take the raw data and store it in fedX
+//the ports I accpet sends data and not really files. Well name meta bytes files and store em
+//I think my ports I send em through have spc metabytes(Oil) Ima just make those files/data/bits/etc into more files
+//should mention encryt... whateva I get is what I get. Need a socket it to read data and check
+
+    nil
+       
+}
+
+func fedX(){//this gone write from the array
+
+    nil
+    
 }
