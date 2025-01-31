0e6df43ff7ae6d7b2ebb1de3218f0d2eb21a603e
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-01-31 11:41:56 -0600

    Update helperFuncs.go

--- helperFuncs.go
+++ helperFuncs.go
@@ -27,13 +27,28 @@ func check16Sum(msg []byte/*workin on the math here...*/){
 //this made for 2 bytes
     //lets kep it even bc pwer of 2 will save my life and think i gotta convert odd numbers
     for := 0; n < len(msg); n += 2{
-        sum += int(msg[n]) * 256 + int(msg[n+1])
+        sum += int(msg[n]) * 128 + int(msg[n+1])
     }
     sum = (sum >> 16) + (sum & 0xffff)
     sum += (sum >> 16)
-    var answer uint16 = uint16(^sum)
+    var answer uint16 = uint16(^sum)//16**
     return answer
 }
+
+check32Sum(msg []byte){
+
+    sum:=0
+    for := 0; n < len(msg); n +=2{
+
+        sum += int(msg[n]) * 256 + int(msg[n+1])        
+       }
+    sum = (sum >> 32) + (sum & 0xffff)
+    sum += (sum >> 16)
+           var answer uint32 = uint16(^sum) 
+           return answer
+}
+
+
 
 /*
 
